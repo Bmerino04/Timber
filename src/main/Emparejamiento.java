4357b1eb81248fe624bccdf8b537d16f39e5c145
ValentinaC <v.cifuentes04@ufromail.cl>
2024-10-04 12:44:38 -0300

    se crea la clase Emparejamiento

---
+++ src/main/Emparejamiento.java
@@ -0,0 +1,21 @@
+public class Emparejamiento {
+
+	private int candidatos;
+	private int idUsuario;
+
+	public void verificarMatch() {
+		// TODO - implement Emparejamiento.verificarMatch
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarCadidatos() {
+		// TODO - implement Emparejamiento.mostrarCadidatos
+		throw new UnsupportedOperationException();
+	}
+
+	public void darLike() {
+		// TODO - implement Emparejamiento.darLike
+		throw new UnsupportedOperationException();
+	}
+
+}
