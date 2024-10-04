cb1be2be0653753927005562d504695c05001e4b
Bmerino04 <bmerinomunoz0404@gmail.com>
2024-10-04 12:25:30 -0300

    add usuario.java

---
+++ src/main/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+
+	private int idUsuario;
+	private int fechaNacimiento;
+	private int email;
+	private int contrasennia;
+	private int perfil;
+
+	public void validarInformacion() {
+		// TODO - implement Usuario.validarInformacion
+		throw new UnsupportedOperationException();
+	}
+
+	public void iniciarSesion() {
+		// TODO - implement Usuario.iniciarSesion
+		throw new UnsupportedOperationException();
+	}
+
+	public void registrarUsuario() {
+		// TODO - implement Usuario.registrarUsuario
+		throw new UnsupportedOperationException();
+	}
+
+	public void registrarPreferencias() {
+		// TODO - implement Usuario.registrarPreferencias
+		throw new UnsupportedOperationException();
+	}
+
+}
