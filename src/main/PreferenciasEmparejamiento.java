ce6f83f6b17aea366ccd9f3c09d9453d3bdd2473
vivivi <v.castro05@ufromail.cl>
2024-10-10 23:33:51 -0300

    se crea el metodo generoCompatible

--- src/main/PreferenciasEmparejamiento.java
+++ src/main/PreferenciasEmparejamiento.java
@@ -4,6 +4,9 @@ public class PreferenciasEmparejamiento {
 	private String[] generoPreferido;
 	private String ciudadPreferida;
 
+	public PreferenciasEmparejamiento(String [] generoPreferido){
+		this.generoPreferido = generoPreferido;
+	}
 
 	public boolean ciudadCompatible(Usuario usuario){
 		return ciudadPreferida.equals(usuario.getCiudadResidencia()) ;
@@ -13,4 +16,15 @@ public class PreferenciasEmparejamiento {
 		int edadUsuario = usuario.calcularEdad();
 		return (edadUsuario >= edadMinima && edadUsuario >= edadMaxima);
 	}
+	
+	public boolean generoCompatible(Usuario usuario){
+		String generoUsuario = usuario.getGenero();
+		for (String genero : generoPreferido){
+			if (genero.equals(generoUsuario)){
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
