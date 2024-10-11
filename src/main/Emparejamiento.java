04c9d6800d201e935713a4bb9c7d2b53e6b8678b
ValentinaC <v.cifuentes04@ufromail.cl>
2024-10-10 21:41:18 -0300

    se añade la logica en darLike()

--- src/main/Emparejamiento.java
+++ src/main/Emparejamiento.java
@@ -2,21 +2,27 @@ import java.util.*;
 
 public class Emparejamiento {
 
-Collection<Usuario> candidatos;
+    Collection<Usuario> candidatos;
 
-public void verificarMatch() {
-// TODO - implement Emparejamiento.verificarMatch
-throw new UnsupportedOperationException();
-}
+    public Emparejamiento(Collection<Usuario> candidatos) {
+        this.candidatos = candidatos;
+    }
 
-public void mostrarCadidatos() {
-// TODO - implement Emparejamiento.mostrarCadidatos
-throw new UnsupportedOperationException();
-}
+    public boolean verificarMatch(Usuario usuario1, Usuario usuario2){
+        return usuario1.getLikesRecibidos().contains(usuario2.getIdUsuario()) &&
+        usuario2.getLikesRecibidos().contains(usuario1.getIdUsuario());
+    }
 
-public void darLike() {
-// TODO - implement Emparejamiento.darLike
-throw new UnsupportedOperationException();
-}
+    public void mostrarCadidatos() {
+
+    }
+
+    public void darLike(Usuario usuarioActual, Usuario candidato) {
+        candidato.anniadirLike(usuarioActual.getIdUsuario());
+        if (verificarMatch(usuarioActual, candidato)){
+            candidato.anniadirMatch(usuarioActual.getIdUsuario());
+            usuarioActual.anniadirMatch(candidato.getIdUsuario());
+        }
+    }
 
 }
