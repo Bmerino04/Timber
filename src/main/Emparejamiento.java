80fe2d16622fff5545f125f68dfa61efc41e6b10
ValentinaC <v.cifuentes04@ufromail.cl>
2024-10-10 21:04:25 -0300

    se añade la logica en verificarMatch()

--- src/main/Emparejamiento.java
+++ src/main/Emparejamiento.java
@@ -1,21 +1,22 @@
+import java.util.*;
+
 public class Emparejamiento {
 
-	private int candidatos;
-	private int idUsuario;
+Collection<Usuario> candidatos;
 
-	public void verificarMatch() {
-		// TODO - implement Emparejamiento.verificarMatch
-		throw new UnsupportedOperationException();
-	}
+public void verificarMatch() {
+// TODO - implement Emparejamiento.verificarMatch
+throw new UnsupportedOperationException();
+}
 
-	public void mostrarCadidatos() {
-		// TODO - implement Emparejamiento.mostrarCadidatos
-		throw new UnsupportedOperationException();
-	}
+public void mostrarCadidatos() {
+// TODO - implement Emparejamiento.mostrarCadidatos
+throw new UnsupportedOperationException();
+}
 
-	public void darLike() {
-		// TODO - implement Emparejamiento.darLike
-		throw new UnsupportedOperationException();
-	}
+public void darLike() {
+// TODO - implement Emparejamiento.darLike
+throw new UnsupportedOperationException();
+}
 
 }
