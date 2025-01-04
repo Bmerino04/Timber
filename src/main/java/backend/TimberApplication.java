91812b4d92648eda1980e6e199fceaf3db41aa12
ValentinaC <v.cifuentes04@ufromail.cl>
2025-01-04 14:12:52 -0300

    springboot initializr

---
+++ src/main/java/backend/TimberApplication.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimberApplication.class, args);
+	}
+
+}
