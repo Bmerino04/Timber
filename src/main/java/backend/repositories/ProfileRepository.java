69c220fc2f189832b093a9121baebd8691980f18
CarlosCienfuegos1 <c.cienfuegos01@ufromail.cl>
2025-01-09 19:42:30 -0300

    Update ProfileRepository.java

--- src/main/java/backend/repositories/ProfileRepository.java
+++ src/main/java/backend/repositories/ProfileRepository.java
@@ -1,10 +1,11 @@
 package backend.repositories;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import backend.entities.User;
+import backend.entities.Profile;
 
 @Repository
-public interface ProfileRepository extends JpaRepository<User, Long>{
-
+public interface ProfileRepository extends JpaRepository<Profile, Long>{
+    Profile findByUserId(Long userId);
 }
